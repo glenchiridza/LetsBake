d19e637ba2d3a87fa489c8b194fc4713e56a5c21
glenchiridza <glenchiridz@gmail.com>
2021-07-24 15:46:17 +0200

    tablayout

--- app/src/main/java/com/glencconnnect/bakingapp/fragments/ViewPagerFragment.kt
+++ app/src/main/java/com/glencconnnect/bakingapp/fragments/ViewPagerFragment.kt
@@ -3,17 +3,19 @@
  **/
 package com.glencconnnect.bakingapp.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.glencconnnect.bakingapp.R
 import com.glencconnnect.bakingapp.models.Recipe
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.fragment_viewpager.*
 
 class ViewPagerFragment:Fragment() {
 
@@ -47,7 +49,17 @@ class ViewPagerFragment:Fragment() {
                 return if (position == 0) ingredientsFragment else directionsFragment
             }
 
+
         }
+
+        val tablayout = view.findViewById<TabLayout>(R.id.tablayout)
+
+        tablayout.setTabTextColors(Color.GRAY,Color.WHITE)
+        TabLayoutMediator(tablayout,viewpager){tab, position ->
+            tab.text = if (position == 0) "Ingredients" else "Directions"
+
+        }.attach()
+
 
         return view
     }
