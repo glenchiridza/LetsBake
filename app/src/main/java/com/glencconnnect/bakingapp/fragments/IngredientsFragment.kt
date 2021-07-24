9fce357c3e118530f797b3c57896a7e3b1e9bcab
glenchiridza <glenchiridz@gmail.com>
2021-07-24 15:14:43 +0200

    ingredients fragment

---
+++ app/src/main/java/com/glencconnnect/bakingapp/fragments/IngredientsFragment.kt
@@ -0,0 +1,23 @@
+/**
+ * Created by glenc on Jul 2021
+ **/
+package com.glencconnnect.bakingapp.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.glencconnnect.bakingapp.R
+
+class IngredientsFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view:View = inflater.inflate(R.layout.fragment_ingredients,container,false)
+        return view
+    }
+}
--- app/src/main/java/com/glencconnnect/bakingapp/fragments/ViewPagerFragment.kt
+++ app/src/main/java/com/glencconnnect/bakingapp/fragments/ViewPagerFragment.kt
@@ -9,6 +9,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.glencconnnect.bakingapp.R
 import com.glencconnnect.bakingapp.models.Recipe
 
@@ -28,6 +31,24 @@ class ViewPagerFragment:Fragment() {
         activity?.title = Recipe.names[index!!]
         val view:View = inflater.inflate(R.layout.fragment_viewpager,container,false)
 
+        val ingredientsFragment = IngredientsFragment()
+
+        val directionsFragment = DirectionsFragment()
+
+        val viewpager = view.findViewById<ViewPager2>(R.id.viewpager)
+
+        viewpager.adapter = object: FragmentStateAdapter(this@ViewPagerFragment){
+            override fun getItemCount(): Int {
+                return 2
+            }
+
+            override fun createFragment(position: Int): Fragment {
+
+                return if (position == 0) ingredientsFragment else directionsFragment
+            }
+
+        }
+
         return view
     }
 
@@ -35,4 +56,5 @@ class ViewPagerFragment:Fragment() {
         super.onStop()
         activity!!.title = resources.getString(R.string.app_name)
     }
+
 }
