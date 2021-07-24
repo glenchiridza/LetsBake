90cd1c6b9dc4a5db5c87d7d4b767e18efe683ef4
glenchiridza <glenchiridz@gmail.com>
2021-07-24 14:15:15 +0200

    recipe title viewpager

--- app/src/main/java/com/glencconnnect/bakingapp/MainActivity.kt
+++ app/src/main/java/com/glencconnnect/bakingapp/MainActivity.kt
@@ -10,16 +10,22 @@ import com.glencconnnect.bakingapp.fragments.ViewPagerFragment
 import com.glencconnnect.bakingapp.models.Recipe
 
 class MainActivity : AppCompatActivity(),ListFragment.OnRecipeSelectedInterface {
+    companion object{
+        val LIST_FRAGMENT =  "list_fragment"
+        val VIEWPAGER_FRAGMENT =  "viewpager_fragment"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val savedFragment = supportFragmentManager.findFragmentById(R.id.value_holder)
+//        val savedFragment = supportFragmentManager.findFragmentById(R.id.value_holder)
+        val savedFragment = supportFragmentManager.
+                    findFragmentByTag(LIST_FRAGMENT)
         if(savedFragment == null) {
             val fragment = ListFragment()
             val fragmentManager: FragmentManager = supportFragmentManager
             val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
-            fragmentTransaction.add(R.id.value_holder, fragment)
+            fragmentTransaction.add(R.id.value_holder, fragment, LIST_FRAGMENT)
             fragmentTransaction.commit()
         }
 
@@ -27,10 +33,14 @@ class MainActivity : AppCompatActivity(),ListFragment.OnRecipeSelectedInterface 
 
     override fun onRecipeSelected(index: Int) {
         val fragment = ViewPagerFragment()
+        val bundle = Bundle()
+        bundle.putInt(ViewPagerFragment.KEY_RECIPE_INDEX,index)
+        fragment.arguments = bundle
         val fragmentManager: FragmentManager = supportFragmentManager
         val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.value_holder, fragment)
-        fragmentTransaction.commit()gti
-        Toast.makeText(this@MainActivity,Recipe.names[index],Toast.LENGTH_SHORT).show()
+        fragmentTransaction.replace(R.id.value_holder, fragment, VIEWPAGER_FRAGMENT)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+
     }
 }
--- app/src/main/java/com/glencconnnect/bakingapp/fragments/ViewPagerFragment.kt
+++ app/src/main/java/com/glencconnnect/bakingapp/fragments/ViewPagerFragment.kt
@@ -7,17 +7,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.glencconnnect.bakingapp.R
+import com.glencconnnect.bakingapp.models.Recipe
 
 class ViewPagerFragment:Fragment() {
+
+    companion object{
+        val  KEY_RECIPE_INDEX = "recipe_index"
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val index = arguments?.getInt(KEY_RECIPE_INDEX)
+//        Toast.makeText(activity, Recipe.names[index!!], Toast.LENGTH_SHORT).show()
+        activity?.title = Recipe.names[index!!]
         val view:View = inflater.inflate(R.layout.fragment_viewpager,container,false)
+
         return view
+    }
+
+    override fun onStop() {
+        super.onStop()
+        activity!!.title = resources.getString(R.string.app_name)
     }
 }
